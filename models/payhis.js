69c195cb39ac68bc9875e705a4224694f519c6cc
GnaneshwariP <35681445+GnaneshwariP@users.noreply.github.com>
2018-01-30 12:31:27 +0530

    added new model payhis

---
+++ models/payhis.js
@@ -0,0 +1,31 @@
+var mongoose=require('mongoose');
+var bcrypt=require('bcryptjs');
+var mongoose=require('mongoose');
+var mongo=require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+
+var permission=mongoose.connect('mongodb://localhost:27017/contact',{
+    useMongoClient:true,
+});
+
+
+var UserSchema= mongoose.Schema({
+    transactionid:{
+        type:String,
+        index:true
+    },
+    date:{
+        type:String
+    },
+    Deviceid:{
+        type:String
+    },
+    amount:{
+        type:String
+    }
+
+});
+
+var User2=module.exports=mongoose.model('User2',UserSchema);
+
+        
