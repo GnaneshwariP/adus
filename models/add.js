6f877120c93d26f817792d1b94a9c5eac0ae4304
Nidheesh <35659549+NidheeshJP@users.noreply.github.com>
2018-02-05 12:20:00 +0530

    changed schema
    total amount and balance amount data type is now number

--- models/add.js
+++ models/add.js
@@ -1,3 +1,5 @@
+
+
 var mongoose=require('mongoose');
 var bcrypt=require('bcryptjs');
 var mongoose=require('mongoose');
@@ -8,32 +10,6 @@ var permission=mongoose.connect('mongodb://localhost:27017/contact',{
     useMongoClient:true,
 });
 var url = "mongodb://localhost:27017/";
-
-
-
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("contact");
-   var myobj = [
-    
-    { devicename: 'abc', amount: '10000', emi:'2000'},
-    { devicename: 'def', amount: '15000', emi:'3000'},
-    { devicename: 'ijk', amount: '20000', emi:'4000'},
-    { devicename: 'xyz', amount: '10000', emi:'2000'},
-    { devicename: 'pqr', amount: '15000', emi:'3000'},
-    { devicename: 'mno', amount: '10000', emi:'4000'},
-
-
-   
-  ];
-  dbo.collection("pre").insertMany(myobj, function(err, res) {
-    //if (err) throw err;
-    //console.log("Number of documents inserted: " );
-    db.close();
-  });
-});
-
 
 
 
@@ -56,10 +32,10 @@ var UserSchema= mongoose.Schema({
         type:String
     },
    totalamount:{
-      type:String
+      type:Number
     },
     balanceamount:{
-      type:String
+      type:Number
     },
      chargepd:{
       type:String
@@ -75,20 +51,14 @@ module.exports.createUser=function(newUser,callback){
            newUser.save(callback);
         };
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+   //update the balance amount when paid
+module.exports.update_doc=function(dev,pd_amt,callback){
+  var query={DeviceId:dev}
+              User1.update(query,{$inc:{balanceamount:-pd_amt}},callback);
+}
 
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("contact");
-  
-  var result= dbo.collection("user1").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
+
 
 
 
