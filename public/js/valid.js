1c293d1f5ac833b308ef6e9240e70d2561ff5e29
Megha <35684661+MeghaN28@users.noreply.github.com>
2018-02-28 16:19:00 +0530

    valid.js files

---
+++ public/js/valid.js
@@ -0,0 +1,3 @@
+<script src="http://code.jquery.com/jquery.min.js"></script>
+    <script src="http://1000hz.github.io/bootstrap-validator/dist/validator.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
