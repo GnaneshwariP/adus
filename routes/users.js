188e5bde5f2496f986663f7e040f8de89211524e
manukumarr <35681618+manukumarr@users.noreply.github.com>
2018-03-21 09:54:41 +0530

    angular routes added

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,149 @@
+// new angular routes
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
+
+// angular register
+router.post('/register', (req, res, next) => {
+  let newUser = new User ({
+    name: req.body.name,
+   username: req.body.username,
+   email: req.body.email,
+    password: req.body.password,
+    password2: req.body.password2,
+    security: req.body.security,
+    answer:req.body.answer
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: 'Failed to register user'});
+    } else {
+      res.json({success: true, msg: 'User registered'});
+    }
+  });
+});
+
+
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return res.json({success: false, msg: 'User not found'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign({ data: user }, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+        res.json({
+          success: true,
+          token: 'JWT '+token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            security:user.security,
+            answer:user.answer
+          }
+        })
+      } else {
+        return res.json({success: false, msg: 'Wrong password'});
+      }
+    });
+  });
+});
+
+
+
+router.post('/add_customer', (req, res, next) => {
+  let newUser = new User1 ({
+     customername: req.body.customername,
+     DeviceName: req.body.DeviceName,
+     DeviceId: req.body.DeviceId,
+     Dop: req.body.Dop,
+    totalamount: req.body.totalamount,
+    balanceamount: req.body.balanceamount,
+    chargepd: req.body.chargepd
+  });
+
+  User.addUser1(newUser, (err, user1) => {
+    if(err) {
+      res.json({success: false, msg: 'Failed to add customer'});
+    } else {
+      res.json({success: true, msg: 'customer is added',
+      token: 'JWT '+token,
+      user1: {
+        id: user._id,
+        
+        customername: user1.customername,
+        Devicename: user1.Devicenamename,
+        DeviceId: user1.DeviceId,
+        Dop: user1.Dop,
+        totalamount: user1.totalamount,
+        balanceamount: user1.balanceamount,
+        chargepd: user1.chargepd
+      }
+    });
+    }
+  });
+});
+
+
+
+
+//login
+router.get('/login', (req, res, next) => {
+  res.send('login');
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
+});
+
+
+//module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old routes non angular
 var express=require('express');
 var exec=require('exec');
 var router=express.Router();
@@ -616,6 +762,5 @@ function ensureAuthenticated(req,res,next){
         res.redirect('/users/login');
     }
 }
-
 
 module.exports=router;
