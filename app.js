804fc408eb717056c7d82a47ee30ce0a092c6532
manukumarr <35681618+manukumarr@users.noreply.github.com>
2018-03-21 10:35:46 +0530

    commented old code

--- app.js
+++ app.js
@@ -1,3 +1,77 @@
+
+
+const express  = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+//connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database'+config.database);
+});
+
+//on error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error'+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port number
+const port = 4000;
+
+//CORS Middleware
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+//passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/login', (req, res) => {
+  res.send('login');
+});
+
+
+//index route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+//start server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
+});
+
+
+/* old app.js non angular
 var express=require('express');
 var path=require('path');
 var cookieParser=require('cookie-parser');
@@ -71,6 +145,8 @@ app.use(expressValidator({
 
 
 
+
+
 app.use(flash());
 
 app.use(function(req,res,next)
@@ -89,3 +165,5 @@ app.set('port',(process.env.PORT||3000));
 app.listen(app.get('port'),function(){
     console.log('server started on port '+app.get('port'));
 });
+
+*/
