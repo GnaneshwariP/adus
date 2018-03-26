f073365372b256b5954b63e4bcfc3a066f7bfaf4
manukumarr <35681618+manukumarr@users.noreply.github.com>
2018-03-26 09:31:42 +0530

    add customer back end storage added

--- angular-src/src/app/components/add-customer/add-customer.component.ts
+++ angular-src/src/app/components/add-customer/add-customer.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
-
+import {ValidateService} from '../../services/validate.service';
+import {AuthService} from '../../services/auth.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-customer',
@@ -9,9 +12,25 @@ import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 })
 export class AddCustomerComponent implements OnInit {
  
+  customername: String;
+  Devicename: String;
+  DeviceId: String;
+  Dop: String;
+  totalamount: String;
+  balanceamount: String;
+  chargepd: String;
+  
+  
+  
+  
 datePickerConfig:Partial<BsDatepickerConfig>;
 
-  constructor() {
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.datePickerConfig=Object.assign({},
     {
       minDate:new Date(1976,0,1),
@@ -24,5 +43,40 @@ datePickerConfig:Partial<BsDatepickerConfig>;
 
   ngOnInit() {
   }
+  
+ onAddCustomerSubmit(){
+    const user1 = {
+      customername: this.customername,
+      Devicename: this.Devicename,
+      DeviceId: this.DeviceId,
+      Dop: this.Dop,
+      totalamount: this.totalamount,
+      balanceamount:this.balanceamount,
+      chargepd: this.chargepd
+      
+    }
+    
+    //required fields
+   if(!this.validateService.validateAddCustomer(user1)){
+    this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+    return false;
+  }
+
+
+
+//add customer
+this.authService.add_customerUser1(user1).subscribe(data => {
+  console.log(data);
+  if(data.success) {
+    this.flashMessage.show('customer added', {cssClass: 'alert-success', timeout: 3000});
+    this.router.navigate(['/dashboard']);
+  } else {
+    this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+    this.router.navigate(['/add_customer']);
+  }
+});
+
+
+  }   
 
 }
