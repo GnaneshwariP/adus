77f560be585040a53f3253db622152f5a4559725
Nidheesh <35659549+NidheeshJP@users.noreply.github.com>
2018-03-26 10:09:07 +0530

    Update edit-customers.component.ts

--- angular-src/src/app/components/edit-customers/edit-customers.component.ts
+++ angular-src/src/app/components/edit-customers/edit-customers.component.ts
@@ -6,13 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-customers.component.css']
 })
 export class EditCustomersComponent implements OnInit {
-tasks: Task[];
-  constructor(private taskService:CustomerService){
-    this.taskService.getTasks()
-        .subscribe(tasks => {
-            this.tasks = tasks;
-        });
-}
+
+ constructor(){
+   
+ }
 
   ngOnInit() {
   }
