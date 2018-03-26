012747e9fc7fd201b65f9092fdcc29e5f06d0b93
Nidheesh <35659549+NidheeshJP@users.noreply.github.com>
2018-03-26 11:21:44 +0530

    retrievied data from nodejs

---
+++ angular-src/Task.ts
@@ -0,0 +1,10 @@
+export class Task{
+    DeviceId:string;
+    DeviceName:string;
+    Dop:string;
+    balanceamount:string;
+    chargepd:string;
+    customername:string;
+    totalamount:string;
+    isDone: boolean;
+}
