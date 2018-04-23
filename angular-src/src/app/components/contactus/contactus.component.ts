eff2e8763e50ede874c49616dc0b11900111af18
manukumarr <35681618+manukumarr@users.noreply.github.com>
2018-04-23 15:20:10 +0530

    feedback or support page

--- angular-src/src/app/components/contactus/contactus.component.ts
+++ angular-src/src/app/components/contactus/contactus.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {AuthService} from '../../services/auth.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contactus',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
+  DeviceId: String;
+  device_name: String;
+  AgentUsername: String;
+  message: String;
 
-  constructor() { }
+
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  onContactusSubmit(){
+  const contus = {
+    DeviceId: this.DeviceId,
+    device_name: this.device_name,
+    AgentUsername: this.AgentUsername,
+    message: this.message
+   
+  }
+
+   //required fields
+   if(!this.validateService.validateContactus(contus)){
+    this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+    return false;
+  }
+
+
+
+//add customer
+this.authService.contactusContus(contus).subscribe(data => {
+  console.log(data);
+  if(data.success) {
+    this.flashMessage.show('Feedback/message is sent', {cssClass: 'alert-success', timeout: 3000});
+    this.router.navigate(['/contactus']);
+  } else {
+    this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+    this.router.navigate(['/contactus']);
+  }
+});
+
+
+
+}
 
 }
